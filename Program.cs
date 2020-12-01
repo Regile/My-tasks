e406b2e9816b90f93e086a4303a9efcacaadda1d
Regile <75294650+Regile@users.noreply.github.com>
2020-12-01 08:51:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstYask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int a = 0, b = 1000;
+            for (int i = a; i < b; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+
+            }
+            System.Console.WriteLine("Сумма чисел меньших 1000, которые делятся на 3 или 5: " + sum);
+            String str = System.Console.ReadLine();
+        }
+    }
+}
