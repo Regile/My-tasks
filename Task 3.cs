65fd61fefbaaf680c1abde28f391d168f34809a4
Regile <75294650+Regile@users.noreply.github.com>
2020-12-01 10:22:53 +0300

    Add files via upload

---
+++ Task 3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstYask
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            long num = 600851475143;
+            int a = 2;
+            while (true) {
+                if (num % a == 0)
+                {
+                    
+                    num /= a;
+                    if (num == 1)
+                    {
+                        System.Console.WriteLine("Наибольший делитель простого числа: " + a);
+                        break;
+                    }
+                }
+                a += 1;
+            }
+
+            String str = System.Console.ReadLine();
+        }
+    }
+}
