f47f8b1aea4550d24dd233105657aba23b4a58f1
Regile <75294650+Regile@users.noreply.github.com>
2020-12-01 10:03:54 +0300

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FirstYask
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int sum = 0;
+            int a = 1, b = 2;
+            int temp;
+            while (b < 4000000)
+            {
+                if (b % 2 == 0)
+                {
+                    sum += b;
+                    
+                }
+
+                temp = b;
+                b = a + b;
+                a = temp;
+
+            }
+            System.Console.WriteLine("Сумма четных чисел Фиббоначи: " + sum);
+            String str = System.Console.ReadLine();
+        }
+    }
+}
